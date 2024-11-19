35be63c81c7b913cdc1053ba145660a8a6b9d81c
heyfredlynch <fred@fredlynch.org>
2024-11-19 15:19:23 -0600

    Add test page

---
+++ pages/test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+    return (
+      <div className="p-4">
+        <h1 className="text-xl font-bold">Test Page</h1>
+        <p>If you can see this, routing is working!</p>
+      </div>
+    )
+  }
